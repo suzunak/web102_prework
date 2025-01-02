c4b75dc96cbcd89b7c72d176f59b47708c601e25
suzunak <suzunakimura@gmail.com>
2025-01-01 22:36:47 -0500

    Total Games

--- index.js
+++ index.js
@@ -75,7 +75,6 @@ const totalBackers = GAMES_JSON.reduce( (acc, games) => {
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
 contributionsCard.innerHTML = totalBackers.toLocaleString('en-US');
 
-
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
@@ -89,6 +88,9 @@ raisedCard.innerHTML = `$${totalRaised.toLocaleString('en-US')}`;
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
 
+const totalGames = GAMES_JSON.length;
+
+gamesCard.innerHTML = totalGames;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
